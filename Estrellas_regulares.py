0412ced215d5b66e842a1ebf9ab01842b9221a02
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-12-19 15:48:37 +0100

    Completamos la funcion "generar_estrella"

--- Estrellas_regulares.py
+++ Estrellas_regulares.py
@@ -1,7 +1,10 @@
 import turtle
 
 def generar_estrella(n):
-
+    angulo = 180 - (360/n)
+    for _ in range (n):
+        turtle.right(angulo)
+        turtle.forward(200)
 
 if __name__ == '__main__':
     n = int(input("Numero de puntas de la estrella: "))
