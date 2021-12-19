b8059e82006dcbd321e1c1595a598b550a5b59cf
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-12-19 15:57:23 +0100

    Incrementamos la velocidad de la estrella

--- Estrellas_regulares.py
+++ Estrellas_regulares.py
@@ -2,9 +2,11 @@ import turtle
 
 def generar_estrella(n):
     angulo = 180 - (360/n)
-    for _ in range (n):
-        turtle.right(angulo)
+    turtle.speed(0)       # Incrementamos la velocidad
+    for _ in range (n):   # Con este bucle se nos genera la estrella
+        turtle.right(angulo)  
         turtle.forward(200)
+    turtle.done()   # De esta manera no se cierra el archivo al acabar la ejecucion del programa
 
 if __name__ == '__main__':
     n = int(input("Numero de puntas de la estrella: "))
