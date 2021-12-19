f9ba038c8c2524d8f61f037c71cefd85ef15c88f
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-12-19 16:18:21 +0100

    Introducimos la funcion "calcular_angulo"

--- Estrellas_regulares.py
+++ Estrellas_regulares.py
@@ -1,10 +1,18 @@
 import turtle
 
 def generar_estrella(n):
-    angulo = 180 - (360/n)# Esto solo nos vale si n es multiplo de 4  :(
+    
+    def calcular_angulo():
+        angulo = 0
+        if n % 4 == 0:
+            angulo = 180 - (360/n)   # Angulo si n es multiplo de 4
+        else:
+            pass
+        return angulo
+
     turtle.speed(0)       # Incrementamos la velocidad
     for _ in range (n):   # Con este bucle se nos genera la estrella
-        turtle.right(angulo)  
+        turtle.right(calcular_angulo())  
         turtle.forward(200)
     turtle.done()   # De esta manera no se cierra el archivo al acabar la ejecucion del programa
 
